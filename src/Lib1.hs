91335a5dca8d1e4906eec6da0d32c2427b9f4ee5
Mykolas Budrys <mykolas.budrys@gmail.com>
2024-09-17 04:05:11 +0300

    Added bnf file containing domain and grammar, list now contains keywords

--- src/Lib1.hs
+++ src/Lib1.hs
@@ -5,4 +5,5 @@ module Lib1
 -- | This function returns a list of words
 -- to be autocompleted in your program's repl.
 completions :: [String]
-completions = []
+completions = ["upper", "lower", "letter", "digit", "number", "alphanumeric", "char", "string", "title", "rental_store", "catalog", "movie_list", "movie", "year", "genre", "rating", "availability", "customer_database", "customer_list", "customer", "name", "customer_id", "phone_number", "email", "rental_history", "rental_transactions", "rental_list", "rental_record", "movie_title", "rental_date", "due_date", "status", "date"]
+
